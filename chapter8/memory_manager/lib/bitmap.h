210404c0117e77a011d107d5d82cbf0987e382d8
seaswalker <xsdwem7@hotmail.com>
2018-07-31 22:32:33 +0800

    chapter8 bitmap

---
+++ chapter8/memory_manager/lib/bitmap.h
@@ -0,0 +1,21 @@
+#ifndef _LIB_KERNEL_BITMAP_H
+#define _LIB_KERNEL_BITMAP_H
+
+# include "global.h"
+
+# define BITMAP_MASK 1
+
+struct bitmap {
+    uint32_t btmp_bytes_len;
+    uint8_t* bits;
+}
+
+void bitmap_init(struct bitmap* btmap);
+
+void bitmap_scan_test(struct bitmap* btmap, uint32_t bit_idx);
+
+int bitmap_scan(struct bitmap* btmap, uint32_t cnt);
+
+int bitmap_set(struct bitmap* btmap, uint32_t index, int8_t value);
+
+#endif
