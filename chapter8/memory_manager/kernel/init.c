3268def1eb1c9c2f41c5a346764cc3da933d70bc
seaswalker <xsdwem7@hotmail.com>
2018-08-05 20:37:20 +0800

    pretty code

--- chapter8/memory_manager/kernel/init.c
+++ chapter8/memory_manager/kernel/init.c
@@ -1,4 +1,4 @@
-# include "init.h"
+﻿# include "init.h"
 # include "kernel/print.h"
 # include "timer.h"
 # include "memory.h"
@@ -6,7 +6,6 @@
 void init_all() {
     put_str("init_all.\n");
     idt_init();
-    // 避免干扰内存实验
     timer_init();
     mem_init();
 }
